d7249bd9867d294fe29441eac6b5976735024cae
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-03-08 13:24:48 -0300

    feat(Eslint configurated): :sparkles:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333, () => console.log("🎇 Server has started!"));
+app.listen(3333, () => console.log('🎇 Server has started!'));
