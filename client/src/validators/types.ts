c26cde781a401bd84b456b83db6153e5694176af
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-04-02 15:00:24 -0300

    feat(Transactions for filters created): :sparkles:

--- client/src/validators/types.ts
+++ client/src/validators/types.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod';
 
-import { createCategorySchema, createTransactionSchmea } from './schemas';
+import {
+  createCategorySchema,
+  createTransactionSchmea,
+  transactionsFilterSchema,
+} from './schemas';
 
 export type CreateCategoryData = z.infer<typeof createCategorySchema>;
 
 export type CreateTransactionData = z.infer<typeof createTransactionSchmea>;
+
+export type TransactionFilterData = z.infer<typeof transactionsFilterSchema>;
