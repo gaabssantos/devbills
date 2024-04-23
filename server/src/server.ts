7f6d1cffb187b4f99c0095a73b855fc5928ef80f
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-04-23 09:45:45 -0300

    docs(Modify server file): :memo:

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,19 +6,19 @@ import { setupMongo } from './database';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { routes } from './routes';
 
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+  }),
+);
+app.use(routes);
+app.use(errorHandler);
+
 setupMongo().then(() => {
   const port = process.env.PORT || 5000;
-
-  const app = express();
-
-  app.use(express.json());
-  app.use(
-    cors({
-      origin: process.env.FRONT_URL,
-    }),
-  );
-  app.use(routes);
-  app.use(errorHandler);
 
   app.listen(port as number, () =>
     console.log(`🎇 Server has started in port ${port}!`),
